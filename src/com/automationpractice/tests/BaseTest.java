2fa0070e3fc559f8f3781448b9b6a5210b7168bc
DarkoDun <darkopescidp@gmail.com>
2021-11-15 16:19:19 +0100

    Refactoring

--- src/com/automationpractice/tests/BaseTest.java
+++ src/com/automationpractice/tests/BaseTest.java
@@ -1,8 +1,14 @@
 package com.automationpractice.tests;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     ChromeDriver driver;
@@ -19,5 +25,10 @@ public class BaseTest {
     @AfterMethod
     public void tearDown(){
         driver.close();
+    }
+
+    public void takeScreenshot(String string) throws IOException {
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotFile, new File(string));
     }
 }
--- src/com/automationpractice/tests/SearchTest.java
+++ src/com/automationpractice/tests/SearchTest.java
@@ -2,11 +2,12 @@ package com.automationpractice.tests;
 
 import com.automationpractice.pages.SearchPage;
 import org.testng.annotations.Test;
+import java.io.IOException;
 
 public class SearchTest extends BaseTest{
 
     @Test
-    public void searchForChiffonDress() {     // Search for item Printed Chiffon Dress and validating search result.
+    public void searchForChiffonDress() throws IOException {     // Search for item Printed Chiffon Dress and validating search result.
         String expectedSearchResult = "[Printed Chiffon Dress]";
         String unexpectedBugSearchResult = "[Printed Chiffon Dress, Printed Summer Dress]";
 
@@ -18,6 +19,7 @@ public class SearchTest extends BaseTest{
         } catch (AssertionError e) {
           searchPage.assertExpectedSearchResult(unexpectedBugSearchResult);{
               System.out.println("---------Assertion failed, bug still unsolved!!!--------- Search result is: " + unexpectedBugSearchResult);
+              takeScreenshot(".//screenshot/unexpectedSearchResult.png");
         }
       }
     }
